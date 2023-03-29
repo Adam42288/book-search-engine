6a2d4a90ed2ca7fc7b2449cd43ae7ac4896f3e31
adam <asantillana@apple.com>
2023-03-29 15:37:50 -0500

    added queries and mtuations to utils folder

---
+++ client/src/utils/queries.js
@@ -0,0 +1,22 @@
+import {gql} from '@apollo/client';
+
+//        link
+//       title
+//       description
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
